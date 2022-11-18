e0b6a277d9133121ae3d4786f42c594542f773c4
Alexsandro <sandro.alex_@hotmail.com>
2022-11-18 16:03:53 -0300

    chore: add boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { GlobalStyles } from '@/styles';
+import { Header, Orders } from '@/components';
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Header />
+      <Orders />
+    </>
+  );
+}
+
+export default App
